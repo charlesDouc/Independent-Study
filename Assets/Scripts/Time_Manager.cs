9282fbbd30776c7f71fec0cbf0672ff48e0cfdfb
charlesDouc <charles.douc@gmail.com>
2018-09-12 19:16:29 -0400

    Implemented a basic reality switch system.

--- Assets/Scripts/Time_Manager.cs
+++ Assets/Scripts/Time_Manager.cs
@@ -1,36 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class Time_Manager : MonoBehaviour {
 	
 	// public variables -------------------
-	public bool timeStop;
-
+	public bool m_timeStop;
+	public PostProcessingProfile m_stopTimeProfile;
+	public PostProcessingProfile m_defaultProfile;
 
 	// private variables ------------------
-	
-	
+	private PostProcessingBehaviour m_currentProfile;
+	private GameObject m_mainCam;
+
 	// ------------------------------------
 	// Use this for initialization
 	// ------------------------------------
 	void Start () {
 		// Default time is stop
-		timeStop = false;
-	
+		m_timeStop = false;
+
+		// Get the default ppp profile
+		m_mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+		m_currentProfile = m_mainCam.GetComponent<PostProcessingBehaviour>();
 	}
 	
+
 	// ------------------------------------
 	// Update is called once per frame
 	// ------------------------------------
 	void Update () {
 		// If left btn mouse is  pressed
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButtonDown(0)) {
 			// Invert the state of time
-			timeStop = !timeStop;
-		}
+			m_timeStop = !m_timeStop;
 
-
-		
+			// Change the post processing profile of the camera
+			if (m_timeStop) {
+				// If time is stopped
+				m_currentProfile.profile = m_stopTimeProfile;
+				Debug.Log(m_currentProfile);
+			} else {
+				// If time is not stopped
+				m_currentProfile.profile = m_defaultProfile;
+				Debug.Log(m_currentProfile);
+			}
+		}		
 	}
 }
