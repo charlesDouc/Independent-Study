21a6d4bd7ef7796479ef28b5f66bf723faaf3fb1
charlesDouc <charles.douc@gmail.com>
2018-09-11 20:41:22 -0400

    Added some light effects to a night grey box scene.

---
+++ Assets/Scripts/Time_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time_Manager : MonoBehaviour {
+	
+	// public variables -------------------
+	public bool timeStop;
+
+
+	// private variables ------------------
+	
+	
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		// Default time is stop
+		timeStop = false;
+	
+	}
+	
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		// If left btn mouse is  pressed
+		if (Input.GetMouseButton(0)) {
+			// Invert the state of time
+			timeStop = !timeStop;
+		}
+
+
+		
+	}
+}
