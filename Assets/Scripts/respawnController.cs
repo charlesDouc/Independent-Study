0670cdac490d8e69ef31a48ad910d1cb71c737b2
Charles Doucet <charles.douc@gmail.com>
2018-10-03 18:40:10 -0400

    Added Second scene in the prototype.

---
+++ Assets/Scripts/respawnController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnController : MonoBehaviour {
+
+	// public variables -------------------
+	public bool m_fromStartPosition = true;
+	public Transform[] m_respawnPosition;
+	[HideInInspector]public int m_postionIndex;
+	[HideInInspector]public bool m_relocate;
+
+
+	// private variables ------------------
+	private Vector3 m_startPos;
+	private Vector3 m_startRot;
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		// Get the start position of the object
+		m_startPos = gameObject.transform.position;
+		m_startRot = gameObject.transform.localEulerAngles;
+
+		m_relocate = false;
+		
+		//Use for checkpoints
+		m_postionIndex = 0;
+	}
+	
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		// If the object needs to be relocated
+		if (m_relocate)
+			respawn();
+		
+	}
+
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+	private void respawn() {
+		// Check the method for respawn
+		if(m_fromStartPosition) {
+			// Relocate object to start position
+			gameObject.transform.position = m_startPos;
+			gameObject.transform.localEulerAngles = m_startRot;
+
+		} else if (m_respawnPosition[m_postionIndex] != null) {
+			// Relocate object to checkpoint locations
+			gameObject.transform.position = m_respawnPosition[m_postionIndex].position;
+			gameObject.transform.localEulerAngles = m_respawnPosition[m_postionIndex].localEulerAngles;
+		}
+
+		// Clear physics and rotation
+		gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+		Debug.Log("Respawned an object");
+		// Disable respawn
+		m_relocate = false;
+	}
+}
---
+++ Assets/Scripts/triggerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerController : MonoBehaviour {
+
+	// public variables -------------------
+	[Header("Trigger Type")]
+	public bool m_respaw;
+
+
+	// private variables ------------------
+
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		
+	}
+	
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		
+	}
+
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+	// Check what object enters the trigger ---------------------
+	void OnTriggerEnter(Collider col) {
+		
+		// If it's for respawn objects
+		if(m_respaw && col.gameObject.tag == "DynamicObject" || col.gameObject.tag == "Player")
+			respawnObject(col.gameObject);
+			
+		
+	}
+
+	// For respawning objects -----------------------------------
+	private void respawnObject(GameObject target) {
+		// check if it's valid
+		if (!target) 
+			return;
+		
+		// Initiate respawn process
+		target.GetComponent<respawnController>().m_relocate = true;
+	}
+}
