97b94402ed74d1ec4f2ea0e344cb0461c9f0f0c4
Charles Doucet <charles.douc@gmail.com>
2018-10-04 09:34:28 -0400

    Added third room and respawning system.

---
+++ Assets/Scripts/rotatorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatorController : MonoBehaviour {
+
+	public float rotSpeed;
+	[Header("Colliders")]
+	public Collider m_inMovement;
+	public Collider m_static;
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		// Check the state of time
+		if (!globalVariables.timeStopped) {
+			rotate();
+
+		} else {
+			m_inMovement.enabled = false;
+			m_static.enabled = true;
+		}
+
+	}
+
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+	private void rotate() {
+		// Rotate movement
+		transform.Rotate(Vector3.right * rotSpeed);
+
+		// Set the correct collider
+		m_inMovement.enabled = true;
+		m_static.enabled = false;
+	}
+}
--- Assets/Scripts/triggerController.cs
+++ Assets/Scripts/triggerController.cs
@@ -7,6 +7,7 @@ public class triggerController : MonoBehaviour {
 	// public variables -------------------
 	[Header("Trigger Type")]
 	public bool m_respaw;
+	public bool m_checkpoint;
 
 
 	// private variables ------------------
@@ -34,8 +35,12 @@ public class triggerController : MonoBehaviour {
 	void OnTriggerEnter(Collider col) {
 		
 		// If it's for respawn objects
-		if(m_respaw && col.gameObject.tag == "DynamicObject" || col.gameObject.tag == "Player")
+		if(m_respaw && col.gameObject.tag == "DynamicObject" || col.gameObject.tag == "Player" && m_respaw)
 			respawnObject(col.gameObject);
+
+		// If it's for respawn objects
+		if(m_checkpoint && col.gameObject.tag == "Player")
+			changeCheckpoint(col.gameObject);
 			
 		
 	}
@@ -49,4 +54,17 @@ public class triggerController : MonoBehaviour {
 		// Initiate respawn process
 		target.GetComponent<respawnController>().m_relocate = true;
 	}
+
+	// For checkpoint index -----------------------------------
+	private void changeCheckpoint(GameObject target) {
+		// check if it's valid
+		if (!target) 
+			return;
+		
+		// Change index
+		target.GetComponent<respawnController>().m_postionIndex ++;
+		// Destroy trigger
+		Destroy(gameObject);
+	}
 }
+
