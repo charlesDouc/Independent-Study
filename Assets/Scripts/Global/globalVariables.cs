35f7a724b3b9f9eedc197d991924d40da7388b5f
Charles Doucet <charles.douc@gmail.com>
2019-01-06 15:00:58 -0500

    Cleaned Up the project. Restart the project folders in Unity 2018.03.

--- Assets/Scripts/Global/globalVariables.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class globalVariables {
-	
-	// Current time state;
-	public static bool timeStopped;
-}
