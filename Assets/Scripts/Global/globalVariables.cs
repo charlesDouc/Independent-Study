3d1d60321de06a0f61a71f08e0af37664df92066
Charles Doucet <charles.douc@gmail.com>
2019-01-20 15:30:32 -0500

    Added bunch of scripts for the player inputs.

---
+++ Assets/Scripts/Global/globalVariables.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class globalVariables {
+	
+	// Current time state;
+	public static bool timeStopped;
+}
